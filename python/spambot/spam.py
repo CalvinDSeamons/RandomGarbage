3a8fdd9c3908b741948cea7abaa0ccc6cd561dd3
Calvin <cal@Calvins-MacBook-Pro.local>
2025-05-29 13:00:06 -0600

    add md5 hash

--- python/spambot/spam.py
+++ python/spambot/spam.py
@@ -3,6 +3,8 @@ import threading
 from random_word import RandomWords
 import pyautogui
 from pynput import keyboard
+import hashlib
+
 
 kill_switch = False
 r = RandomWords()
@@ -23,11 +25,17 @@ def listen_for_kill_switch():
         listener.join()
 
 # Send word to Messages
-def send_random_word():
+def send_random_word(key):
     word = r.get_random_word()
+    hashing = md5_hash(key,word)
     print(f"Sending: {word}")
-    pyautogui.typewrite(word)
+    pyautogui.typewrite(hashing)
     pyautogui.press('enter')
+
+def md5_hash(key: str, word: str) -> str:
+    combined = (key + word).encode('utf-8')
+    return hashlib.md5(combined).hexdigest()
+
 
 if __name__ == '__main__':
     listener_thread = threading.Thread(target=listen_for_kill_switch)
@@ -36,6 +44,12 @@ if __name__ == '__main__':
     print("Switch to Messages window. Starting in 5 seconds...")
     time.sleep(5)
 
+    key = '95468401-5567-452E-A56B-E6255C34F7C9'
+    phrase = ("MD5-Hashing: " +"["+key+"]")
+    pyautogui.typewrite(phrase)
+    pyautogui.press('enter')
+    time.sleep(2)
+
     while not kill_switch:
-        send_random_word()
-        time.sleep(2)  # Wait 2 seconds between messages
+        send_random_word(key)
+        time.sleep(1)  # Wait 2 seconds between messages
